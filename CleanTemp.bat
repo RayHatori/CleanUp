f741b297c2e4f282d9d30e4767e79351a87af94d
RayHatori <114110811+RayHatori@users.noreply.github.com>
2026-02-27 09:53:06 +0300

    Update CleanTemp.bat

--- CleanTemp.bat
+++ CleanTemp.bat
@@ -1,6 +1,5 @@
 @echo off
 :: CleanTemp.bat
-echo .bat by v33
 echo Cleaning stuff...
 del /q /f /s %TEMP%\* 2>nul
 rd /s /q %TEMP% 2>nul
@@ -8,4 +7,5 @@ mkdir %TEMP% 2>nul
 echo Cleaning Recycle Bin...
 rd /s /q C:\$Recycle.bin 2>nul
 echo Done!
+
 pause
